eefce8683d67fdb47544010f0af334a0ca4a66d5
TigoG <tigogoes11@gmail.com>
2024-04-05 12:52:04 +0200

    added flags

--- dashboard/src/components/ScreenSaver.vue
+++ dashboard/src/components/ScreenSaver.vue
@@ -1,31 +1,99 @@
 <template>
-    <div class="quarter-top-left">
-      <p>{{ text }}</p>
+  <div class="container">
+    <div class="row">
+      <div class="flag-container">
+        <GreenFlag :status="greenFlagStatus" />
+        <div>
+          <p v-if="greenFlagStatus">Green Flag True</p>
+          <p v-else>Green Flag False</p>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      text: {
-        type: String,
-        required: true
-      }
+    <div class="row">
+      <div class="flag-container">
+        <RedFlag :status="redFlagStatus" />
+        <div>
+          <p v-if="redFlagStatus">Red Flag True</p>
+          <p v-else>Red Flag False</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="flag-container">
+        <BlockedFlag :status="blockedFlagStatus" />
+        <div>
+          <p v-if="blockedFlagStatus">Blocked Flag True</p>
+          <p v-else>Blocked Flag False</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GreenFlag from './GreenFlag.vue';
+import RedFlag from './RedFlag.vue';
+import BlockedFlag from './BlockedFlag.vue';
+
+export default {
+  components: {
+    GreenFlag,
+    RedFlag,
+    BlockedFlag,
+  },
+  data() {
+    return {
+      selectedNumbers: ['1', '1', '1'], // Default selected numbers
+      greenFlagStatus: false,
+      redFlagStatus: false,
+      blockedFlagStatus: false
+    };
+  },
+  methods: {
+    selectNumber(index, number) {
+      this.selectedNumbers[index - 1] = number.toString();
+      this.checkFlags();
+    },
+    checkFlags() {
+      this.greenFlagStatus = this.selectedNumbers[0] === '2';
+      this.redFlagStatus = this.selectedNumbers[1] === '2';
+      this.blockedFlagStatus = this.selectedNumbers[2] === '2';
     }
-  };
-  </script>
-  
-  <style scoped>
-  .quarter-top-left {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 50%; /* Adjust as needed */
-    height: 50%; /* Adjust as needed */
-    padding: 20px;
-    background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent black background */
-    color: white;
-    box-sizing: border-box;
   }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.container {
+  border: 2px solid black;
+  padding: 20px; /* Reduced padding */
+}
+
+.row {
+  margin-bottom: 10px;
+}
+
+.flag-container {
+  display: flex;
+  align-items: center;
+}
+
+.flag {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+
+.green {
+  background-color: green;
+}
+
+.red {
+  background-color: red;
+}
+
+.blocked {
+  background-color: gray;
+}
+</style>
